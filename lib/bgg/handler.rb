ced7d6a32e03562b2ff480f119458aa7d0794aa5
Markos Rigas <m.rigas@skroutz.gr>
2021-03-28 22:23:32 +0300

    Add game mechanisms

--- lib/bgg/handler.rb
+++ lib/bgg/handler.rb
@@ -1,18 +1,23 @@
 class Bgg::Handler
   FETCH_COLLECTION_BASE_URL = 'https://www.boardgamegeek.com/xmlapi2/collection'.freeze
-
+  FETCH_BOARDGAME_BASE_URL = 'https://www.boardgamegeek.com/xmlapi2/thing'.freeze
   def self.fetch_user_games(username, filters: {})
     HTTParty.get(FETCH_COLLECTION_BASE_URL + "?username=#{username}&own=1")
     sleep(10)
     bgg_response = HTTParty.get(FETCH_COLLECTION_BASE_URL + "?username=#{username}&own=1")
+    game_ids = bgg_response.to_h.with_indifferent_access[:items][:item].map{|x| x[:objectid]}
+    mechanisms = get_game_mechanisms(game_ids) if filters[:mechs].present? if filters[:mechs].present?
 
     bgg_response.to_h.with_indifferent_access[:items][:item].map do |game|
       {
         name: game[:name][:__content__],
         bgg_id: game[:objectid],
-        plays: game[:numplays].to_i
+        plays: game[:numplays].to_i,
+        mechanisms: mechanisms.select{ |gm| gm[:id] == game[:objectid]}.first[:mechs],
+        rank: mechanisms.select{ |gm| gm[:id] == game[:objectid]}.first[:rank]
       }
     end
+
   end
 
   def self.unplayed_common_games(usernames)
@@ -69,5 +74,17 @@ class Bgg::Handler
 
   def self.top_rated_games(username, limit = 10)
     #TBD
+  end
+
+  def self.get_game_mechanisms(game_ids)
+    bgg_response = HTTParty.get(FETCH_BOARDGAME_BASE_URL + "?id=#{game_ids.join(',')}&stats=1")
+
+    bgg_response.to_h.with_indifferent_access[:items][:item].map do |gm|
+      {
+        id: gm[:id],
+        mechs: gm[:link].select{ |t| t[:type] == "boardgamemechanic"}.map{|b| b[:value] },
+        rank: gm[:statistics][:ratings][:ranks].present? ? gm[:statistics][:ratings][:ranks] : 888888888888
+      }
+    end
   end
 end
